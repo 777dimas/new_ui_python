0994894504aebad1c749126966cf3d9e9a474bec
Dmitry <Dmitry>
2017-11-27 19:52:36 +0200

    centred window

--- login_ui.py
+++ login_ui.py
@@ -3,33 +3,34 @@
 import configparser
 import sys
 
-from PyQt5.QtWidgets import (QPushButton, QLineEdit, QWidget, QApplication)
+import PyQt5.QtWidgets
 
-class Example(QWidget):
+
+class Example(PyQt5.QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        config = configparser.ConfigParser()
-        config.read('launcher.ini')
-        login = config.get('Options', 'Logins')
+     #   config = configparser.ConfigParser()
+     #   config.read('launcher.ini')
+     #   login = config.get('Options', 'Logins')
 
-        name_set = QLineEdit(self)
+        name_set = PyQt5.QtWidgets.QLineEdit(self)
         name_set.move(90, 20)
         name_set.resize(215, 30)
 
-        pass_set = QLineEdit(self)
+        pass_set = PyQt5.QtWidgets.QLineEdit(self)
         pass_set.move(90, 70)
         pass_set.resize(215, 30)
 
-        button_set = QPushButton("Set", self)
+        button_set = PyQt5.QtWidgets.QPushButton("Set", self)
         button_set.setGeometry(380, 70, 90, 30)
-        button_set.clicked.connect(self.buttonSet)
+       # button_set.clicked.connect(self.buttonSet)
         button_set.move(90, 130)
 
-        button_close = QPushButton("Close", self)
+        button_close = PyQt5.QtWidgets.QPushButton("Close", self)
         button_close.setGeometry(380, 70, 90, 30)
         #button_close.clicked.connect(self.closeEvent)
         button_close.move(215, 130)
@@ -38,20 +39,24 @@ class Example(QWidget):
         self.setFixedHeight(200)
         self.setFixedWidth(400)
         self.show()
+        rectangle = self.frameGeometry()
+        centerPoint = PyQt5.QtWidgets.QDesktopWidget().availableGeometry().center()
+        rectangle.moveCenter(centerPoint)
+        self.move(rectangle.topLeft())
 
-    def buttonSet(self):
-        config = configparser.ConfigParser()
-        config.read('launcher.ini')
-        config.set('Options', 'Logins', self.())
-        with open('launcher.ini', 'w') as configfile:
-            config.write(configfile)
+   # def buttonSet(self):
+   #     config = configparser.ConfigParser()
+   #     config.read('launcher.ini')
+   #     config.set('Options', 'Logins', self.())
+   #     with open('launcher.ini', 'w') as configfile:
+   #         config.write(configfile)
 
-    def buttonClose(self):
+   # def buttonClose(self):
 
 
 
 if __name__ == '__main__':
 
-    app = QApplication(sys.argv)
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
