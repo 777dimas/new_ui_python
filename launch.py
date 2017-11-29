60f40af50942acd30d85a48a18cfbde6851ecb7a
Dmitry <Dmitry>
2017-11-29 16:17:15 +0200

    add variable serial

--- launch.py
+++ launch.py
@@ -3,5 +3,6 @@ import configparser, subprocess
 config = configparser.ConfigParser()
 config.read('launcher.ini')
 Login = config.get('Options', 'Logins')
-if not Login:
+serial = config.get('Options', 'serial')
+if not Login or serial:
     subprocess.call("/home/dima/PycharmProjects/new_ui_python/login_ui.py", shell=True)
