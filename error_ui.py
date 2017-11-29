e591a02176c4b48be383e81b5688397d1ac030da
Dmitry <Dmitry>
2017-11-29 22:19:05 +0200

    add error_ui.py

---
+++ error_ui.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys, subprocess
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QDesktopWidget
+
+class ErrorWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        okButton = QPushButton("Ok", self)
+        okButton.setGeometry(120, 10, 90, 30)
+        okButton.clicked.connect(self.close)
+        okButton.move(155, 90)
+
+        self.setWindowTitle('Error')
+        self.nameLabel = QLabel("Enter 'Login', 'Password' and 'Serial'", self)
+        self.nameLabel.move(100, 40)
+        self.setFixedHeight(150)
+        self.setFixedWidth(400)
+        self.center()
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = ErrorWindow()
+    sys.exit(app.exec_())
