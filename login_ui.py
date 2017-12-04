9aa216d48f1ffa7f7245a6e046ce3f548d8a6eb8
Dmitry <kriptexm@gmail.com>
2017-12-04 14:49:53 +0200

    renamed variables

--- login_ui.py
+++ login_ui.py
@@ -14,26 +14,26 @@ class AuthWindow(QWidget):
 
     def initUI(self):
 
-        self.loginField = QLineEdit(self)
-        self.loginField.move(90, 20)
-        self.loginField.resize(215, 30)
-        loginFieldName = QLabel(self)
-        loginFieldName.setText("Login:")
-        loginFieldName.move(48, 25)
+        self.login_field = QLineEdit(self)
+        self.login_field.move(90, 20)
+        self.login_field.resize(215, 30)
+        login_field_name = QLabel(self)
+        login_field_name.setText("Login:")
+        login_field_name.move(48, 25)
 
-        self.passField = QLineEdit(self)
-        self.passField.move(90, 70)
-        self.passField.resize(215, 30)
-        passFieldName = QLabel(self)
-        passFieldName.setText("Password:")
-        passFieldName.move(25, 75)
+        self.pass_field = QLineEdit(self)
+        self.pass_field.move(90, 70)
+        self.pass_field.resize(215, 30)
+        pass_field_name = QLabel(self)
+        pass_field_name.setText("Password:")
+        pass_field_name.move(25, 75)
 
-        self.serialField = QLineEdit(self)
-        self.serialField.move(90, 120)
-        self.serialField.resize(215, 30)
-        serialFieldName = QLabel(self)
-        serialFieldName.setText("Serial:")
-        serialFieldName.move(48, 125)
+        self.serial_field = QLineEdit(self)
+        self.serial_field.move(90, 120)
+        self.serial_field.resize(215, 30)
+        serial_field_name = QLabel(self)
+        serial_field_name.setText("Serial:")
+        serial_field_name.move(48, 125)
 
         button_set = QPushButton("Apply", self)
         button_set.setGeometry(380, 70, 90, 30)
@@ -54,29 +54,26 @@ class AuthWindow(QWidget):
 
     def center(self):
 
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
+        windowGeometry = self.frameGeometry()
+        windowCenter = QDesktopWidget().availableGeometry().center()
+        windowGeometry.moveCenter(windowCenter)
+        self.move(windowGeometry.topLeft())
 
     def writeLogins(self):
 
-        login = self.loginField.text()
-        password = self.passField.text()
-        serial = self.serialField.text()
+        login = self.login_field.text()
+        password = self.pass_field.text()
+        serial = self.serial_field.text()
         if not login or not password or not serial:
-            subprocess.call("/home/kriptex/PycharmProjects/new_ui_python/error_ui.py", shell=True)
+            subprocess.call("/home/dima/PycharmProjects/new_ui_python/error_ui.py", shell=True)
         else:
-            loginValue = (login+','+password)
+            login_value = (login+','+password)
             config = configparser.ConfigParser()
             config.read('launcher.ini')
-            config.set('Options', 'Logins', loginValue)
-            config.set('Options', 'serial', serial)
+            config.set('Options', 'Autologin', login_value)
+            config.set('Options', 'Serial', serial)
             with open('launcher.ini', 'w') as configfile:
                 config.write(configfile)
-                if not login or password or serial:
-                    subprocess.call("error.py")
        # subprocess.call("cd /home/flash/Progs/GlobalSlots/ && wine launcher.exe", shell=True)
 
 if __name__ == '__main__':
