a579239149592e8f71df6c6475a53cf3c198cad8
Dmitry <kriptexm@gmail.com>
2017-12-25 11:22:19 +0200

    add "while True"

--- launch.py
+++ launch.py
@@ -4,7 +4,8 @@ config = configparser.ConfigParser()
 config.read('launcher.ini')
 auto_login = config.get('Options', 'Autologin')
 serial = config.get('Options', 'Serial')
-if not auto_login or not serial:
+if auto_login or serial:
+    while True:
+        subprocess.call("cd /home/dima/PycharmProjects/new_ui_python/ && wine launcher.exe", shell=True)
+else:
     subprocess.call("./login_ui.py", shell=True)
-#else:
- #   subprocess.call("cd /home/dima/PycharmProjects/new_ui_python/ && wine launcher.exe", shell=True)
