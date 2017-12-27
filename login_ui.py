a736bb938e526e6b8321d2ea36078e671af23611
Anton Smorodskyi <asmorodskyi@suse.com>
2017-12-27 19:28:26 +0100

    Refactor and code clean up

--- login_ui.py
+++ login_ui.py
@@ -1,86 +1,78 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import configparser, subprocess
-import sys
+import configparser, subprocess, sys, time
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QDesktopWidget
 
+
 class AuthWindow(QWidget):
+    BUTTON_SIZES = [380, 70, 90, 30]
 
     def __init__(self):
         super().__init__()
         self.config = configparser.ConfigParser()
         self.config.optionxform = str
         self.config.read('launcher.ini')
-        self.initUI()
 
-    def initUI(self):
+        self.login_edit = self.build_edit(90, 20, 215, 30)
+        self.login_label = self.build_label("Login:", 48, 25)
 
-        self.loginField = QLineEdit(self)
-        self.loginField.move(90, 20)
-        self.loginField.resize(215, 30)
-        loginFieldName = QLabel(self)
-        loginFieldName.setText("Login:")
-        loginFieldName.move(48, 25)
+        self.pass_edit = self.build_edit(90, 70, 215, 30)
+        self.pass_label = self.build_label("Password:", 25, 75)
 
-        self.passField = QLineEdit(self)
-        self.passField.move(90, 70)
-        self.passField.resize(215, 30)
-        passFieldName = QLabel(self)
-        passFieldName.setText("Password:")
-        passFieldName.move(25, 75)
+        self.serial_edit = self.build_edit(90, 120, 215, 30)
+        self.serial_edit.setText(self.config.get('Options', 'Serial'))
+        self.serial_label = self.build_label("Serial:", 48, 125)
 
-        self.serialField = QLineEdit(self)
-        self.serialField.move(90, 120)
-        self.serialField.resize(215, 30)
-        serialFieldName = QLabel(self)
-        serialFieldName.setText("Serial:")
-        serialFieldName.move(48, 125)
-        self.serialField.setText(self.config.get('Options', 'Serial'))
-
-        button_set = QPushButton("Apply", self)
-        button_set.setGeometry(380, 70, 90, 30)
-        button_set.clicked.connect(self.writeLogins)
-        button_set.clicked.connect(self.close)
-        button_set.clicked.connect(self.startProgramm)
-        button_set.move(90, 180)
-
-        closeButton = QPushButton("Close", self)
-        closeButton.setGeometry(380, 70, 90, 30)
-        closeButton.clicked.connect(self.close)
-        closeButton.move(215, 180)
+        self.apply_btn = self.build_button("Apply", 90, 180, self.write_logins)
+        self.close_btn = self.build_button("Close", 215, 180)
 
         self.setWindowTitle('- Autorization - ')
         self.setFixedHeight(250)
         self.setFixedWidth(400)
-        self.center()
+        window_geometry = self.frameGeometry()
+        window_geometry.moveCenter(QDesktopWidget().availableGeometry().center())
+        self.move(window_geometry.topLeft())
         self.show()
 
-    def center(self):
+    def build_label(self, label_text, label_pos_x, label_pos_y):
+        qlabel = QLabel(self)
+        qlabel.setText(label_text)
+        qlabel.move(label_pos_x, label_pos_y)
+        return qlabel
 
-        windowGeometry = self.frameGeometry()
-        windowCenter = QDesktopWidget().availableGeometry().center()
-        windowGeometry.moveCenter(windowCenter)
-        self.move(windowGeometry.topLeft())
+    def build_edit(self, edit_x, edit_y, edit_pos_x, edit_pos_y):
+        qline_edit = QLineEdit(self)
+        qline_edit.move(edit_x, edit_y)
+        qline_edit.resize(edit_pos_x, edit_pos_y)
+        return qline_edit
 
-    def writeLogins(self):
-        if self.loginField.text() and self.passField.text() and self.serialField.text():
-            loginValue = (self.loginField.text() + ',' + self.passField.text())
-            self.config.set('Options', 'Autologin', loginValue)
-            self.config.set('Options', 'Serial', self.serialField.text())
+    def build_button(self, btn_text, btn_pos_x, btn_pos_y, connect_method=None):
+        qbutton = QPushButton(btn_text, self)
+        qbutton.setGeometry(self.BUTTON_SIZES[0], self.BUTTON_SIZES[1], self.BUTTON_SIZES[2], self.BUTTON_SIZES[3])
+        if connect_method is not None:
+            qbutton.clicked.connect(connect_method)
+        qbutton.clicked.connect(self.close)
+        qbutton.move(btn_pos_x, btn_pos_y)
+        return qbutton
+
+    def write_logins(self):
+        if self.login_edit.text() and self.pass_edit.text() and self.serial_edit.text():
+            login_and_pass_str = (self.login_edit.text() + ',' + self.pass_edit.text())
+            self.config.set('Options', 'Autologin', login_and_pass_str)
+            self.config.set('Options', 'Serial', self.serial_edit.text())
             with open('launcher.ini', 'w') as configfile:
                 self.config.write(configfile)
         else:
             subprocess.call("./error_ui.py", shell=True)
-
-    def startProgramm(self):
-        if self.loginField.text() and self.passField.text() and self.serialField.text():
+        if self.login_edit.text() and self.pass_edit.text() and self.serial_edit.text():
             while True:
-               subprocess.call("./wine launcher.exe", shell=True)
+                subprocess.call("wine launcher.exe", shell=True)
+                time.sleep(5)
+
 
 if __name__ == '__main__':
-
     app = QtWidgets.QApplication(sys.argv)
     ex = AuthWindow()
     sys.exit(app.exec_())
